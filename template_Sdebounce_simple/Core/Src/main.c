4372903b88fba505242cc8c49ff021d3be376dbe
AndreaCastronovo <andreacastro@live.it>
2021-07-01 16:23:31 +0200

    Prova per verifica git

--- template_Sdebounce_simple/Core/Src/main.c
+++ template_Sdebounce_simple/Core/Src/main.c
@@ -1,7 +1,7 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.c
+  * @file           : main.c  1 2 3 4
   * @brief          : Campionamento accellerometro a 100Hz con applicazione di filtro FIR passa
   * 	              basso per filtrare ciascun asse dell'accellerometro a 4Hz. Calcolo
   * 	              orientamento planare e accensione del led più vicino al suolo.
